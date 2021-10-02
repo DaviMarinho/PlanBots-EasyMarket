5d582a3cbf39990e787f5353167f6bd553669dbd
nYCSTs <lucascandradex14@gmail.com>
2021-10-02 20:48:22 -0300

    Finalizada controller de usuario. Adicionada atribuicao de ID da loja ao usuario quando esta for criada

--- Backend/src/Controllers/UsersController.js
+++ Backend/src/Controllers/UsersController.js
@@ -1,45 +1,76 @@
 const User = require('../Models/UserSchema');
 const bcrypt = require('bcrypt');
+const validate = require('../Utils/validate');
 
 const createUser = async (req, res) => {
-    const {
-        userName, email, phone, pass, storeID, rating, cpf,
-    } = req.body;
+  const {
+    username, email, phone, pass, cpf,
+  } = req.body;
 
-    const validFields = validation.validateUser(
-        userName, email, phone, pass, storeID, rating, cpf,
-    );
+  const invalidValues = validate.validateValues(username, email, phone, pass, cpf);
 
-    if (validFields.length) {
-        return res.status(400).json({ status: validFields });
-    }
+  if (invalidValues.length) {
+    return res.status(400).json({ 'invalid values': invalidValues });
+  }
 
-    var salt = bcrypt.genSaltSync(10);
-    var finalPass = bcrypt.hashSync(pass, salt);
+  var salt = bcrypt.genSaltSync(10);
+  var cryptedPassword = bcrypt.hashSync(pass, salt);
 
-    try {
-        const user = await User.create({
-            userName, 
-            email, 
-            phone, 
-            finalPass, 
-            storeID, 
-            rating, 
-            cpf,
-        });
-        return res.json(user);
-    } catch (error) {
-        return res.status(400).json({ duplicated: error.keyValue });
-    }
+  try {
+    const user = await User.create({
+      username,
+      email,
+      phone,
+      cryptedPassword,
+      storeID: '',
+      rating: 0,
+      cpf,
+    });
+    return res.json(user);
+  } catch (error) {
+    return res.status(400).json({ duplicated: error.keyValue });
+  }
 }
 
 const editUser = async (req, res) => {
-}
+  const { id } = req.params;
+  const {
+    username, email, phone, pass, cpf,
+  } = req.body;
 
+  const invalidValues = validate.validateValues(username, email, phone, pass, cpf);
+
+  if (invalidValues.length) {
+    return res.status(400).json({ 'invalid values': invalidValues });
+  }
+
+  var salt = bcrypt.genSaltSync(10);
+  var cryptedPassword = bcrypt.hashSync(pass, salt);
+
+  try {
+    const updatedUser = await User.findOneAndUpdate({ _id: id }, {
+      username,
+      email,
+      phone,
+      cryptedPassword,
+      cpf,
+    }, { new: true });
+    return res.json(updatedUser);
+  } catch (err) {
+    return res.json(err);
+  };
+};
 
 const deleteUser = async (req, res) => {
-}
+  const { id } = req.params;
+  try {
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+    return res.json(deletedUser);
+  } catch (err) {
+    return res.json(err);
+  };
+};
 
 module.exports = {
-    createUser, editUser, deleteUser,
+  createUser, editUser, deleteUser,
 };
