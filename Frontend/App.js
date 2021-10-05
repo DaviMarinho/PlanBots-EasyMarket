cf762c695cf5d25ec0e1ab37d44e30a236c8077f
nYCSTs <lucascandradex14@gmail.com>
2021-10-05 15:37:01 -0300

    Ajustes na pagina de login. Adicao de dependencias e adicao da rota de registro no App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomePage from './src/scenes/homepage';
 import showStore from './src/scenes/showStore';
 import Login from './src/scenes/login';
+import Signup from './src/scenes/signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,6 +23,10 @@ const App = () => {
         <Stack.Screen
           name="login"
           component={Login}
+        />
+        <Stack.Screen
+          name="register"
+          component={Signup}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- Frontend/src/scenes/login/index.js
+++ Frontend/src/scenes/login/index.js
@@ -2,39 +2,51 @@ import React, { useState } from 'react'
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import Navbar from '../../components/navbar';
 import Header from '../../components/header';
+import { loginUser } from '../../services/apiservices';
 
 const Login = ({ navigation }) => {
-  const [username, setUsername] = useState('');
+  const [userdata, setUserdata] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const login = async () => {
+    await loginUser(email, password)
+    .then((r) => setUserdata(r.data));
+  }
 
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
         <Text style={styles.header}>Login</Text>
-        <Text style={styles.label}>Email:</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setUsername}
-          value={username}
-          placeholder="Email"
-        />
-        <Text>Senha:</Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setPassword}
-          value={password}
-          placeholder="Senha"
-        />
-        <View>
-          <Text>
+        <View style={styles.inputs}>
+          <Text style={styles.label}>Email:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Email"
+          />
+          <Text style={styles.label}>Senha:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setPassword}
+            value={password}
+            placeholder="Senha"
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button onPress={() => login()} color='rgb(117,136,236)' title="Entrar" />
+        </View>
+        <View style={styles.additionalOptions}>
+          <Text style={styles.additionalOptionsText}>
             Esqueci minha senha
           </Text>
-          <Text>
+          <Text style={styles.additionalOptionsText} onPress={() => navigation.navigate('register')}>
             Criar conta
           </Text>
         </View>
-        <Button title="Entrar" />
       </View>
       <Navbar />
     </View>
@@ -53,22 +65,40 @@ const styles = StyleSheet.create({
     paddingTop: 12,
     paddingBottom: 12,
   },
-  label: {
-    textAlign: 'left',
-    alignItems: 'flex-start',
-  },
   content: {
-    alignItems: 'center',
     paddingTop: 12,
     paddingBottom: 12,
   },
+  label: {
+    fontWeight: 'bold',
+  },
   input: {
     height: 40,
-    margin: 12,
+    marginTop: 12,
+    marginBottom: 12,
     borderWidth: 1,
     padding: 10,
-    width: '90%',
+    borderRadius: 8
   },
+  inputs: {
+    paddingRight: 12,
+    paddingLeft: 12,
+  },
+  additionalOptions: {
+    marginTop: 12,
+    marginBottom: 12,
+    alignItems: 'center',
+  },
+  additionalOptionsText: {
+    fontWeight: 'bold',
+    marginTop: 6,
+    marginBottom: 6,
+    fontSize: 16,
+    padding: 6,
+  },
+  button: {
+    alignItems: 'center',
+  }
 });
 
 export default Login;
