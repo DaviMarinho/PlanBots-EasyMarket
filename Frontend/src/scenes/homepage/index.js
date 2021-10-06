be1fe1952ce804b99bf78d9feb42fc74407e72e9
DaviMarinho <davii_marinho@hotmail.com>
2021-10-05 23:53:23 -0300

    Adiciona o mapa na página inicial
    Signed-off-by: DaviMarinho <davii_marinho@hotmail.com>

--- Frontend/src/scenes/homepage/index.js
+++ Frontend/src/scenes/homepage/index.js
@@ -1,21 +1,35 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React from 'react'
-import { StyleSheet, StatusBar, View, Text, Button, TextInput } from 'react-native';
-import Navbar from '../../components/navbar';
+import React, { useEffect, useState } from "react";
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 
-const HomePage = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Button title="Ola" onPress={() => navigation.navigate('showStore')}>EasyMarket!</Button>
-      <Button title="Limpar" onPress={() => AsyncStorage.clear()}></Button>
-      <StatusBar style="auto" />
-      <View style={styles.teste}>
-        <Text>Oi</Text>
+const HomePage = () => {
+const [region, setRegion] = useState(null);
+useEffect(() => {
+  Location.installWebGeolocationPolyfill()
+  navigator.geolocation.getCurrentPosition(
+    // success
+    async ({ coords: { latitude, longitude } }) => {
+      setRegion({
+        latitude,
+        longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    },
+    // error
+    () => {},{
+      timeout: 2000,
+      enableHighAccuracy: true,
+      maximumAge: 1000,
+    });
+  }, []);
+return (
+      <View style={styles.container}>
+        <MapView style={styles.mapStyle} initialRegion={region}/>
       </View>
-      <Navbar />
-    </View>
-  );
-};
+    );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -24,9 +38,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  teste: {
-    backgroundColor: 'blue'
-  }
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
 
 export default HomePage;
