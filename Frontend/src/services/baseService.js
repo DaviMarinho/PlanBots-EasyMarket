472e43f60f2a195b3a2723cddbe1619746376b75
thiagof99 <thiago3399@hotmail.com>
2021-10-24 21:39:00 -0300

    ultimos ajustes
    Signed-off-by: thiagof99 <thiago3399@hotmail.com>

--- Frontend/src/services/baseService.js
+++ Frontend/src/services/baseService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const APIGeneral = axios.create({ baseURL: `http://192.168.0.102:3000` });
+const APIGeneral = axios.create({ baseURL: `` });
 
 export default APIGeneral;
   
